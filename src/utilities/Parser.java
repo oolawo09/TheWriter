01415184e754aa07d5359fd00f9390802f54c99c
anthony <anthony.olawo@duke.edu>
2014-05-05 04:45:31 -0400

    final update to code

--- src/utilities/Parser.java
+++ src/utilities/Parser.java
@@ -47,7 +47,7 @@ public class Parser {
 	 * @return
 	 */
 	
-	public List<String> breakFileInputIntoStrings(){ 
+	private List<String> breakFileInputIntoStrings(){ 
 		String [] temp = parsedString.split("[?.!]");
 		for(int i= 0; i<temp.length; i++){
 			sentences.add(temp[i]);
